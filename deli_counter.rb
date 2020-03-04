fe529f6b6ff5d9d639e44b29082f7507fa220bfe
Mark Stabler <mark@stablerwritingservices.com>
2020-03-04 23:26:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,21 +2,21 @@
 
 katz_deli = []
 
-line(line)
+def line(line)
 end
 
-take_a_number(line, name)
-	line.push(name)
-	puts "Welcome, #{name}. You are number #{line.size} in line.
-	line (don't think I need this)
+def take_a_number(line, name)
+  line.push(name)
+  puts "Welcome, #{name}. You are number #{line.size} in line."
+  line # (don't think I need this)
 end
 
-now_serving(line)
-	if line.size == 0
-		puts "There is nobody waiting to be served!".
-	else
-		puts "Currently serving #{line.first}."
-		line.shift
-		line (don't think I need this)
-	end
+def now_serving(line)
+  if line.size == 0
+    puts "There is nobody waiting to be served!".
+  else
+    puts "Currently serving #{line.first}."
+    line.shift
+    line # (don't think I need this)
+  end
 end
