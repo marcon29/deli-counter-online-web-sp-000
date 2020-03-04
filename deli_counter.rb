7f96f3777942f690e9fb3f007a1871f2a49e6df5
Mark Stabler <mark@stablerwritingservices.com>
2020-03-04 23:48:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -28,11 +28,19 @@ end
 
 def now_serving(line)
   if line.size == 0
-    puts "There is nobody waiting to be served!".
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{line.shift}."
     # line # (don't think I need this)
   end
+end
+
+
+if deliq.size == 0
+  puts "There is nobody waiting to be served!"
+else
+  puts "Currently serving #{deliq.shift}."
+  # line # (don't think I need this)
 end
 
 deliq = ["Ada", "Grace", "Kent"]
