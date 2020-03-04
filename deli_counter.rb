7738e806c651c9b74530cfe0bb7444437233dee2
Mark Stabler <mark@stablerwritingservices.com>
2020-03-04 23:34:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,13 @@
 katz_deli = []
 
 def line(line)
+  if line.size == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{line.join}"
+    # puts "The line is currently: #{index}. #{name}"
+    # (this needs iteration with index)
+  end
 end
 
 def take_a_number(line, name)
